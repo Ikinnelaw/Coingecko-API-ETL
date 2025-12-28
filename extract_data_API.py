132bf57a2c8e602ff956c83456d772e26e717a88
Erick Diaz <erick.diazzamora@gmail.com>
2025-12-27 22:58:11 -0600

    Cambio a solo 3 criptomonedas para evitar saturar la API

--- extract_data_API.py
+++ extract_data_API.py
@@ -1,4 +1,4 @@
-import requests
+import requests , time
 import logging
 from datetime import datetime, timezone
 
@@ -8,7 +8,7 @@ API_TRENDING = "https://api.coingecko.com/api/v3/search/trending"
 API_MARKET = "https://api.coingecko.com/api/v3/coins/{}/market_chart"
 API_MARKET_GLOBAL = "https://api.coingecko.com/api/v3/global"
 
-cryptos = ['bitcoin', 'ethereum', 'tether', 'binancecoin', 'solana']
+cryptos = ['bitcoin', 'ethereum', 'tether']
 currencies = ['usd','eur','mxn']
 HEADERS = {"accept": "application/json"}
 
@@ -43,6 +43,8 @@ def get_prices():
             print(f"Datos obtenidos: {len(simple_data)} criptomonedas")
             print(f"Cryptos: {list(simple_data.keys())}")
             print("Precios de las criptomonedas:", simple_data)
+
+            return simple_data
         else:
             print(f"Error en la API de precios: {response_price.status_code}")
     except requests.exceptions.Timeout:
@@ -53,77 +55,122 @@ def get_prices():
 # Función para obtener detalles de la criptomoneda
 def get_details():
     try:
+
+        resultados = {}
+
         for crypto in cryptos:
-            response_detailed = requests.get(f"{API_URL_DETAILED}{crypto}", params=parametros_detalle, timeout=10)
-            
-            if response_detailed.status_code == 200:
-                data = response_detailed.json()
-                print(f"\nDetalles de {crypto}:")
-                print(f"Nombre: {data['name']}")
-                print(f"Símbolo: {data['symbol']}")
-                print(f"Categorías: {', '.join(data['categories'])}")
-                print(f"Descripción: {data['description']['en']}")
+            response = requests.get(f"{API_URL_DETAILED}{crypto}",params=parametros_detalle,timeout=10)
+
+            if response.status_code == 200:
+                resultados[crypto] = response.json()
+                #print(f"JSON para {crypto}: {resultados[crypto]}")
+
+            elif response.status_code == 429:
+                logger.warning(f"Rate limit alcanzado para {crypto}")
+
             else:
-                print(f"Error en la API de detalles para {crypto}: {response_detailed.status_code}")
+                logger.error(f"Error {response.status_code} para {crypto}")
+
+            time.sleep(1.5)  # nos esperamos un minuto
+
+        return resultados if resultados else None 
+            
     except requests.exceptions.Timeout:
         logger.error("Error: La solicitud ha superado el tiempo de espera")
     except Exception as e:
-        logger.error(f"Ocurrió un error inesperado: {e}")
+        logger.error(f"Ocurrió un error en get_details: {e}")
+        return None
 
 
 # Función para obtener los exchanges
 def get_exchanges():
-    for crypto in cryptos:
-        try:
-            response = requests.get(f"{API_URL_DETAILED}{crypto}/tickers", timeout=10)
-            if response.status_code == 200:
-                tickers_data = response.json()
-                if 'tickers' in tickers_data and tickers_data['tickers']:
-                    print(f"\nExchanges para {crypto}:")
 
-                    count = 0  # Contador para limitar a 10 resultados cada moneda para evitar que saturemos la API
-                    for exchange in tickers_data['tickers']:
-                        if count == 10:  # Si ya se han mostrado 10, sale del bucle
-                            break
-                        print(f"  Exchange: {exchange['market']['name']}")
-                        print(f"  Par de comercio: {exchange['market']['identifier']}")
-                        print(f"  Precio: {exchange.get('last', 'No disponible')} USD")
-                        count += 1  # Incrementar el contador
-                else:
-                    print(f"No hay exchanges disponibles para {crypto}")
+    try:
+        resultados = {} 
+    
+        for crypto in cryptos:
+            response = requests.get(
+                f"{API_URL_DETAILED}{crypto}/tickers",
+                timeout=10
+            )
+
+            if response.status_code == 200:
+                data = response.json()
+
+                tickers_limitados = []
+                contador = 0
+
+                for ticker in data.get("tickers", []):
+
+                    if contador == 5:
+                        break
+
+                    tickers_limitados.append(ticker)
+                    contador += 1
+
+                # Reemplazamos los tickers originales por los limitados
+                data["tickers"] = tickers_limitados
+
+                resultados[crypto] = data
+
+                print(f"JSON para {crypto}: {resultados[crypto]}")
             else:
-                print(f"Error {response.status_code} al obtener exchanges para {crypto}")
-        except requests.exceptions.Timeout:
-            print("Error: La solicitud ha superado el tiempo de espera")
-        except Exception as e:
+                logger.error(f"Error {response.status_code} para {crypto}")
+
+            time.sleep(1.5)  # evita 429 (rate limit)
+
+        return resultados if resultados else None
+        
+    except Exception as e:
             print(f"Ocurrió un error inesperado: {e}")
 
 # Función para obtener el historial de mercado
 def get_market_history():
     try:
-        # recorremos cada moneda  de nuestra lista para obtener su market 
+        resultados = {}
+
         for crypto in cryptos:
             market_url = API_MARKET.format(crypto)
-            response_market = requests.get(market_url, params=params_market, timeout=10)
-            
+            response_market = requests.get(market_url,params=params_market,timeout=10)
+
             if response_market.status_code == 200:
                 data_market = response_market.json()
-                print(f"\nHistorial del mercado para {crypto}:")
-                print("Primeros 5 datos del historial de precios (últimos 30 días):")
-                position = 1
-                
-                #Validamos sl precio 
-                for entry in data_market['prices']:
-                    if position > 5:  # Limita la iteración a los primeros 5 elementos
+
+                precios_limitados = []
+                contador = 0
+
+                for entry in data_market.get("prices", []):
+
+                    if contador == 5:
                         break
-                    timestamp = entry[0]
-                    price = entry[1]
-                    print(f"Posición {position} - Fecha: {timestamp}, Precio: {price} USD")
-                    position += 1
+
+                    precios_limitados.append({
+                        "timestamp": entry[0],
+                        "price_usd": entry[1]
+                    })
+
+                    contador += 1
+
+                resultados[crypto] = precios_limitados
+            
+                #Para debug ver los resultados 
+                #print(f"JSON para {crypto}: {resultados[crypto]}")
+                print(f"{crypto} → precios cargados: {contador}")
+            
+                time.sleep(1.5)  # evitar 429
+            
             elif response_market.status_code == 429:
-                print(f"Demasiadas solicitudes a la API, esperando antes de volver a intentar con {crypto}...")
+                logger.warning(
+                    f"Rate limit alcanzado para {crypto}, esperando..."
+                )
+                time.sleep(5)
+
             else:
-                print(f"Error al obtener el historial de mercado de {crypto}: {response_market.status_code}")
+                logger.error(
+                    f"Error {response_market.status_code} al obtener market de {crypto}"
+                )
+
+        return resultados if resultados else None
     except requests.exceptions.Timeout:
         logger.error("Error: La solicitud ha superado el tiempo de espera")
     except Exception as e:
@@ -146,6 +193,8 @@ def get_trending():
                 print(f"Símbolo: {coin['symbol']}")
                 print(f"ID: {coin['id']}")
                 position += 1
+
+            return trending_data
         else:
             print(f"Error en la API de tendencias: {response_trending.status_code}")
     except requests.exceptions.Timeout:
@@ -159,11 +208,15 @@ def get_global_market():
         response_market_global = requests.get(API_MARKET_GLOBAL, headers=HEADERS, timeout=10)
         
         if response_market_global.status_code == 200:
+
             logger.info("Global market extraído")
             data_global = response_market_global.json()
             print(f"La data global es: \n {data_global}")
             time_now = datetime.now(timezone.utc)
             print(f"El tiempo actual es : {time_now}")
+
+            return data_global
+        
         else:
             print(f"Error global market: {response_market_global.status_code}")
     except requests.exceptions.Timeout:
@@ -173,12 +226,14 @@ def get_global_market():
 
 # Función principal para llamar todas las funciones
 def extract_data():
-    #get_prices()
-    #get_details()
-    get_exchanges()
-    #get_market_history()
-    #get_trending()
-    #get_global_market()
+    return {
+        #"prices": get_prices(),
+        #"details": get_details(),
+        #"exchanges": get_exchanges(),
+        #"market_history": get_market_history(),
+        #"trending": get_trending(),
+        #"global_market": get_global_market()
+    }
 
 # Ejecutar el script
 extract_data()
