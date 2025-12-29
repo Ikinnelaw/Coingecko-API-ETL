1d21d6d5d2ce00e8365b8de0e6682bd27916413e
Erick Diaz <erick.diazzamora@gmail.com>
2025-12-29 17:18:00 -0600

    Se agrego el archivo conf para conectar a la DB

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 DB_config = {
-    'host': 'localhost',
-    'user' : 'kali',
-    'password': 'Kali1416',
-    'database' : 'crypto_tracker'
+    'host': 'localhost', # Poner la ip de tu servidor BD 
+    'user' : 'youruser', # pon aqui el usuario de tu DB
+    'password': 'yourpassword', # Agrega aqui tu contraseña
+    'database' : 'crypto_tracker' #El nombre que quieras que tenga la DB lo puedes cambiar por otro
 }
